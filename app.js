b6643158eac9bca8272ed7a92490afcbc0d79040
sunyou10 <sy021023@gmail.com>
2024-05-28 01:11:38 +0900

    05/28 commit -cha

--- app.js
+++ app.js
@@ -73,6 +73,55 @@ async function createReview(restaurantId, newReview) {
     updateStarRate(restaurantId);
 }
 
+// 레스토랑 예약 내역 추가하는 함수
+async function createReserve(restaurantId, newReserve) {
+    const restaurants = await fetchAllRestaurants();
+    const restaurantIndex = restaurants.findIndex(r => r.id === String(restaurantId));
+    if (restaurantIndex === -1) throw new Error('Restaurant not found');
+
+    // 이전의 예약과 중복 확인
+    const reservations = restaurants[restaurantIndex].reservations;
+    reservations.forEach(reservation => {
+        if (reservation.date === newReserve.date && reservation.time === newReserve.time) throw new Error('A reservation at the same time');
+    })
+
+    reservations.push(newReserve);
+
+    await fs.writeFile(RESTAURANT_JSON_FILENAME, JSON.stringify(restaurants, null, '\t'));
+    console.log(`New Reservation! : ${restaurants[restaurantIndex].name}`);
+}
+
+// 레스토랑 예약 내역 삭제하는 함수
+async function deleteReserve(restaurantId, userId) {
+    const restaurants = await fetchAllRestaurants();
+    const restaurantIndex = restaurants.findIndex(r => r.id === String(restaurantId));
+    if (restaurantIndex === -1) throw new Error('Restaurant not found');
+
+    let reservations = restaurants[restaurantIndex].reservations;
+    const reservation = reservations.filter(r => r.email === userId);
+    reservation.forEach(r => {
+        reserveIndex = reservations.indexOf(r);
+        console.log(reserveIndex);
+        reservations.splice(restaurantIndex, 1);
+    })
+    await fs.writeFile(RESTAURANT_JSON_FILENAME, JSON.stringify(restaurants, null, '\t'));
+    console.log('Delete Completion!');
+}
+
+// 일괄 삭제하는 함수
+async function clearReserve(restaurantId, userId) {
+    const restaurants = await fetchAllRestaurants();
+    const restaurantIndex = restaurants.findIndex(r => r.id === String(restaurantId));
+    if (restaurantIndex === -1) throw new Error('Restaurant not found');
+
+    let reservations = restaurants[restaurantIndex].reservations;
+    restaurants[restaurantIndex].reservations = reservations.filter(r => r.email !== userId);
+
+    await fs.writeFile(RESTAURANT_JSON_FILENAME, JSON.stringify(restaurants, null, '\t'));
+    console.log('Delete Completion!');
+}
+
+// 메일 보내는 함수
 const sendMail = async (to, subject, html) => {
     const googleTransporter = await nodemailer.createTransport({
         service: 'gmail',
@@ -111,10 +160,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
-
-app.get('/location', (req, res) => {
-    res.sendFile(__dirname + '/location.html');
-});
 
 // 로그인
 app.get('/login', (req, res) => {
@@ -155,7 +200,7 @@ app.post('/submit-your-findPw-form', async (req, res) => {
         res.json({ success: false });
         return;
     } else {
-        if (user.nickname === nickname) {
+        if (user.nickname === nickname && user['phone-number'] === phoneNum) {
             res.json({ success: true, password: `${user.password}` });
             return;
         } else {
@@ -353,25 +398,39 @@ app.post('/reserve', async (req, res) => {
     // 현재 로그인한 사용자 정보 가져오기 (쿠키 또는 세션에서)
     const userCookie = req.cookies[USER_COOKIE_KEY];
     if (!userCookie) {
-        res.status(403).json({ success: false, message: "로그인이 필요합니다." });
+        res.status(200).json({ success: false, message: "로그인이 필요합니다." });
         return;
     }
 
     const user = JSON.parse(userCookie);
 
-    // user.json 파일을 읽어와서 해당 사용자의 예약 정보에 추가
-    const users = await fetchAllUsers();
-    const userIndex = users.findIndex(u => u.id === user.id);
-    if (userIndex !== -1) {
-        if (!users[userIndex].reservations) {
-            users[userIndex].reservations = [];
-        }
-        users[userIndex].reservations.push(reservation);
+    if(reservation.email === undefined){
+        reservation.email = user.id;
+        reservation.name = user.nickname;
+        reservation.phone = user["phone-number"];
+    }
 
-        await fs.writeFile(USERS_JSON_FILENAME, JSON.stringify(users, null, 2));
-        res.json({ success: true });
-    } else {
-        res.status(404).json({ success: false, message: "사용자를 찾을 수 없습니다." });
+    // 중복 검사 이후, restaurant.json에 예약 정보 추가
+    try {
+        await createReserve(reservation.restaurantId, reservation);
+        // user.json 파일을 읽어와서 해당 사용자의 예약 정보에 추가
+        const users = await fetchAllUsers();
+        const userIndex = users.findIndex(u => u.id === user.id);
+        if (userIndex !== -1) {
+            if (!users[userIndex].reservations) {
+                users[userIndex].reservations = [];
+            }
+            users[userIndex].reservations.push(reservation);
+
+            await fs.writeFile(USERS_JSON_FILENAME, JSON.stringify(users, null, 2));
+            res.json({ success: true });
+        } else {
+            res.status(404).json({ success: false, message: "사용자를 찾을 수 없습니다." });
+        }
+    }
+    catch(error) { 
+        console.log(`${error}`);
+        res.status(200).json({ success: false, message: "[중복알림] 해당 일시엔 예약이 불가능합니다." });
     }
 });
 
@@ -522,7 +581,7 @@ app.get('/time-to-reservation', async (req, res) => {
         return;
     }
 
-    const reservation = user.reservations[0];
+    const reservation = user.reservations[user.reservations.length -1];
     const reservationDate = moment(`${reservation.date} ${reservation.time}`, 'YYYY-MM-DD HH:mm');
     const now = moment();
 
@@ -548,26 +607,80 @@ async function saveUsers(users) {
     await fs.writeFile('public/js/data/user.json', JSON.stringify(users, null, 2), 'utf-8');
 }
 
-// 예약 일괄 삭제
+// 해당 레스토랑 관련 예약 일괄 삭제
 app.post('/delete-all-reservations', async (req, res) => {
     const email = req.body.email;
+    const restaurantId = req.body.restaurantId;
     const data = await fs.readFile('public/js/data/user.json');
     const users = JSON.parse(data);
 
     const user = users.find(user => user.id === email);
 
     if (!user || !user.reservations || user.reservations.length === 0) {
-        res.status(400).json({ message: '예약한 정보가 없어요.' });
+        res.status(200).json({ success:false, message: '예약한 정보가 없어요.' });
         return;
     }
     
     if (user) {
+        await deleteReserve(restaurantId, email);
+        reservations = user.reservations;
+        const reservation = reservations.filter(r => r.restaurantId === restaurantId);
+            reservation.forEach(r => {
+            reserveIndex = reservations.indexOf(r);
+            reservations.splice(reserveIndex, 1);
+        })
+        await saveUsers(users);
+        res.json({ success: true, message: '다음에 다시 예약해주세요~!!!' });
+    } else {
+        res.status(404).json({ success: false, message: 'User not found.' });
+    }
+});
+
+app.post('/clear-all-reservations', async (req, res) => {
+    const email = req.body.email;
+    const data = await fs.readFile('public/js/data/user.json');
+    const users = JSON.parse(data);
+    const user = users.find(user => user.id === email);
+
+    // 해당 사용자의 예약 정보가 존재하지 않는 경우
+    if (!user || !user.reservations || user.reservations.length === 0) {
+        res.status(200).json({ success:false, message: '예약한 정보가 없어요.' });
+        return;
+    }
+    if (user) {
+        const restaurants = await fetchAllRestaurants();
+        HasReserve = restaurants.filter(r => r.reservations.length > 0);
+        const reservations = HasReserve.map(r => r.reservations).flat();
+        
+        for (const reservation of reservations) {
+            if (reservation.email === email) {
+                await clearReserve(reservation.restaurantId, email);
+            }
+        }
+
         user.reservations = [];
         await saveUsers(users);
         res.json({ success: true, message: '다음에 다시 예약해주세요~!!!' });
     } else {
         res.status(404).json({ success: false, message: 'User not found.' });
     }
+});
+
+app.post('/api/restaurants', async (req, res) => {
+    const { searchInput, selectedTags, selectedPriceRange, parkingAvailable } = req.body;
+    const restaurants = await fetchAllRestaurants();
+
+    const filteredData = restaurants.filter(restaurant => {
+        const matchesName = restaurant.name.toLowerCase().includes(searchInput);
+        const matchesLocation = restaurant.location.toLowerCase().includes(searchInput);
+        const matchesTags = selectedTags.length === 0 || selectedTags.some(tag => restaurant.tag.includes(tag));
+        const matchesPrice = !selectedPriceRange || restaurant.priceRange === selectedPriceRange;
+        const matchesParking = !parkingAvailable || restaurant.additionalServices.includes("주차 가능");
+
+        return (matchesName || matchesLocation) && matchesTags && matchesPrice && matchesParking;
+    });
+
+    res.json(filteredData);
 });
 
 app.listen(port, () => console.log(`Page open in  port: ${port}`));
